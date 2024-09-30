121ba26e84df5a96477838b9d981c363fb38c9fb
Alexandre Figueiredo <17xande@gmail.com>
2024-09-30 21:15:43 +0200

    finetune codeium plugin

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,6 +1,17 @@
 return {
   'Exafunction/codeium.vim',
+  event = 'BufEnter',
   config = function()
-    vim.g.codeium_manual = true
+    -- vim.g.codeium_manual = true
+    vim.keymap.set('i', '<C-g>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true, silent = true })
+
+    -- vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](0) end, { expr = true, silent = true })
+    -- vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    -- vim.keymap.set('i', '<C-x>', function()
+    --   return vim.fn['codeium#Clear']()
+    -- end, { expr = true, silent = true })
+    vim.g.codeium_no_map_tab = true
   end,
 }
