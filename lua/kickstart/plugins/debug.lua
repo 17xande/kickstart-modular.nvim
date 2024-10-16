08aaf4bb7a964f296703f367b7940bdf91d5d077
Alexandre Figueiredo <17xande@gmail.com>
2024-10-16 14:08:34 +0200

    added python debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -65,6 +65,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'debugpy',
       },
     }
 
@@ -102,5 +103,7 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
   end,
 }
