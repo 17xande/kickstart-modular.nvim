58994212946913fd820e945326ccde8e4272c4b9
Alexandre Figueiredo <17xande@gmail.com>
2024-11-04 08:48:53 +0200

    add todo comments keymaps

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,31 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      {
+        '<leader>]t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next Todo comment',
+      },
+      {
+        '<leader>[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous Todo comment',
+      },
+      {
+        '<leader>st',
+        '<cmd>TodoTelescope<cr>',
+        desc = 'Search Todos in Telescope',
+      },
+    },
+    opts = { signs = true },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
